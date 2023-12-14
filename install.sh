4237efbf70b1e3f5272bcfe1afedce83979a3932
Supavitax <supavitax@gmail.com>
2023-12-14 21:16:49 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,10 +21,10 @@ function preflight_checks {
         exit -1
     fi
 
-    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F 'klipper.service')" ]; then
-        printf "[PRE-CHECK] Klipper service found! Continuing...\n\n"
+    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F 'klipper-2.service')" ]; then
+        printf "[PRE-CHECK] Klipper-2 service found! Continuing...\n\n"
     else
-        echo "[ERROR] Klipper service not found, please install Klipper first!"
+        echo "[ERROR] Klipper-2 service not found, please install Klipper first!"
         exit -1
     fi
 }
